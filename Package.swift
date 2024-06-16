80067af112fbe17a59e7bb088920cb7a2a9c7b29
Sasikumar <jpsasi@gmail.com>
2024-06-16 07:32:49 +0530

    JPAsyncCacheImage added

--- Package.swift
+++ Package.swift
@@ -5,17 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "JPSwiftUIToolkit",
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "JPSwiftUIToolkit",
             targets: ["JPSwiftUIToolkit"]),
     ],
+    dependencies: [
+      .package(url: "https://github.com/jpsasi/JPSwiftToolkit", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "JPSwiftUIToolkit"),
+          name: "JPSwiftUIToolkit", 
+          dependencies: [
+            .product(name: "JPSwiftToolkit", package: "JPSwiftToolkit"),
+          ]),
         .testTarget(
             name: "JPSwiftUIToolkitTests",
             dependencies: ["JPSwiftUIToolkit"]
---
+++ Sources/JPSwiftUIToolkit/JPAsyncCacheImage.swift
@@ -0,0 +1,57 @@
+//
+//  JPAsyncCacheImage.swift
+//
+//
+//  Created by Sasikumar JP on 6/15/24.
+//
+
+import SwiftUI
+import JPSwiftToolkit
+
+public var ImageCache: JPCache<URL, Image> = JPCache()
+
+public struct JPAsyncCacheImage<Content>: View where Content: View {
+  let url: URL
+  let scale: CGFloat
+  let transaction: Transaction
+  let content: (AsyncImagePhase) -> Content
+
+  init(url: URL, scale: CGFloat = 1.0,
+       transaction: Transaction = .init(animation: .easeIn),
+       @ViewBuilder content: @escaping (AsyncImagePhase) -> Content) {
+    self.url = url
+    self.scale = scale
+    self.transaction = transaction
+    self.content = content
+  }
+  
+  public var body: some View {
+    if let image = ImageCache.value(forKey: url) {
+      content(.success(image))
+    } else {
+      AsyncImage(url: url) { phase in
+        cacheAndRender(phase: phase)
+      }
+    }
+  }
+  
+  private func cacheAndRender(phase: AsyncImagePhase) -> some View {
+    if case let .success(image) = phase {
+      ImageCache.insert(image, forKey: url)
+    }
+    return content(phase)
+  }
+}
+
+#Preview {
+  JPAsyncCacheImage(url: URL(string: "https://picsum.photos/200/300" )!, scale: 1.0, transaction: .init(animation: .easeIn)) { phase in
+    if case let .success(image) = phase {
+      image
+        .resizable()
+        .aspectRatio(contentMode: .fill)
+        .ignoresSafeArea()
+    } else {
+      Text("Unable to load Image")
+    }
+  }
+}
